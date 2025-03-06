f1c43dec48e1825a7ba02da5f5f89ae602bd053a
Octavio AR <L21170517@culiacan.tecnm.mx>
2025-03-05 17:42:41 -0700

    Guardar archivos en carpeta Unidad 1

--- ARBOL/nodo.py
+++
@@ -1,6 +0,0 @@
-class Nodo:
-    # Metodo constructor del nodo
-    def __init__(self, dato):
-        self.dato = dato
-        self.izquierda = None
-        self.derecha = None
--- Unidad 1/PROYECTO PUZZLE 8/puzzleOcho.py
+++ Unidad 1/PROYECTO PUZZLE 8/puzzleOcho.py
@@ -5,38 +5,35 @@ import heapq
 import time
 
 class Puzzle:
-    #Constructor de la clase
+
     def __init__(self, estado_inicial, estado_objetivo):
         self.estado_inicial = estado_inicial
         self.estado_objetivo = estado_objetivo
 
     #Fucion para validar si los estados tienen las mismas fichas
     def validar_estados(self):
-        #Convertimos los estados en listas planas
         estado_inicial_flat = [ficha for fila in self.estado_inicial for ficha in fila]
         estado_objetivo_flat = [ficha for fila in self.estado_objetivo for ficha in fila]
 
         return sorted(estado_inicial_flat) == sorted(estado_objetivo_flat)
 
-    #Funcion para validar si el tablero ya está resuelto
     def validar_solucion(self):
         return self.estado_inicial == self.estado_objetivo
 
-    #Funcion para encontrar la posicion de un valor en el estado
     def encontrar_posicion(self, estado, valor):
-        for fila in range(3):
+        for fila in range(3): 
             for columna in range(3):
                 if estado[fila][columna] == valor:
                     return fila, columna
         return None
     
     #Funcion para calcular la distancia manhattan
-    def calcular_distancia_manhattan(self, estado):
+    def distancia_manhattan(self, estado):
         distancia_total = 0
         for fila in range(3):
             for columna in range(3):
                 valor = estado[fila][columna]
-                if valor != 0: #El 0 no se cuenta ya que es el espacio vacio
+                if valor != 0:
                     fila_objetivo, columna_objetivo = self.encontrar_posicion(self.estado_objetivo, valor)
                     distancia_total += abs(fila - fila_objetivo) + abs(columna - columna_objetivo)
         return distancia_total
@@ -44,16 +41,18 @@ class Puzzle:
     #Funcion para obtener los vecinos del estado
     def obtener_vecinos(self, estado):
         vecinos = []
-        fila_blanco, columna_blanco = self.encontrar_posicion(estado, 0)
-        movimientos = [(-1, 0), (1, 0), (0, -1), (0, 1)]  #arriba, abajo, izquierda, derecha
+        fila_vacia, columna_vacia = self.encontrar_posicion(estado, 0)
+        
+        movimientos = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # arriba, abajo, izquierda, derecha
         for mov_fila, mov_columna in movimientos:
-            nueva_fila, nueva_columna = fila_blanco + mov_fila, columna_blanco + mov_columna
+            nueva_fila = fila_vacia + mov_fila
+            nueva_columna = columna_vacia + mov_columna
             if 0 <= nueva_fila < 3 and 0 <= nueva_columna < 3:
-                nuevo_estado = [fila[:] for fila in estado]
-                nuevo_estado[fila_blanco][columna_blanco], nuevo_estado[nueva_fila][nueva_columna] = nuevo_estado[nueva_fila][nueva_columna], nuevo_estado[fila_blanco][columna_blanco]
-                vecinos.append(nuevo_estado)
+                nuevo_estado = [fila[:] for fila in estado] 
+                nuevo_estado[fila_vacia][columna_vacia], nuevo_estado[nueva_fila][nueva_columna] = nuevo_estado[nueva_fila][nueva_columna], nuevo_estado[fila_vacia][columna_vacia]
+                vecinos.append(nuevo_estado)        
         return vecinos
-    
+        
     #Funcion para resolver el puzzle utilizando el algoritmo A* y heuristica manhattan
     def resolver_Puzzle(self):
 
@@ -76,8 +75,8 @@ class Puzzle:
             
             for vecino in self.obtener_vecinos(estado_actual):
                 if tuple(map(tuple, vecino)) not in visitados: # Si el vecino no ha sido visitado se agregará a la lista de prioridad
-                    costo_g = len(camino) + 1
-                    costo_h = self.calcular_distancia_manhattan(vecino)
+                    costo_g = len(camino) + 1 
+                    costo_h = self.distancia_manhattan(vecino)
                     heapq.heappush(lista_prioridad, (costo_g + costo_h, vecino, camino + [estado_actual]))
         return None
 
@@ -88,10 +87,11 @@ def imprimir_estado(estado):
 
 def main():
     estado_inicial = [
-        [0, 8, 7],
-        [6, 5, 4],
-        [3, 2, 1]
+        [1, 0, 3],
+        [4, 8, 7],
+        [2, 6, 5]
     ]
+        
     estado_objetivo = [
         [1, 2, 3],
         [4, 5, 6],
@@ -117,7 +117,7 @@ def main():
             print(f"Paso {paso}:")
             imprimir_estado(estado)
         print(f"Total de movimientos: {len(solucion) - 1}")
-        print(f"Tiempo de ejecucion: {fin_tiempo - inicio_tiempo:.1f} segundos")
+        print(f"Tiempo de ejecucion: {fin_tiempo - inicio_tiempo:.6f} segundos")
     else:
         print("No se encontro solucion")
 
